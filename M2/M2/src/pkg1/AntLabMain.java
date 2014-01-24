1fac707fd50d86268161ba66786a921c41ed084a
Zac <Zach@143.215.50.252>
2014-01-24 15:07:31 -0500

    AntLabMain name change

--- M2/M2/src/pkg1/AntLabMain.java
+++ M2/M2/src/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ public class AntLabMain {
 			toPrint += p4.toString("ncampbell7");
 			//Person5 replace P4 with your name
 			//and gburdell5 with your gt id
-			Person5 p5 = new Person5("P5");
-			toPrint += p5.toString("gburdell5");
+			Person5 p5 = new Person5("Zaccheus Stewart");
+			toPrint += p5.toString("zstewart6");
 
 		System.out.println(toPrint);
 
