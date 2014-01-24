225e3786de13ed6ec1332d28607d8a7c5d67ccd1
Nigel <ncampbell7@gatech.edu>
2014-01-24 12:47:42 -0500

    Minor change

--- M2/M2/src/pkg1/AntLabMain.java
+++ M2/M2/src/pkg1/AntLabMain.java
@@ -70,7 +70,7 @@ public class AntLabMain {
 			//Person4 replace P4 with your name
 			//and gburdell4 with your gt id
 			Person4 p4 = new Person4("Nigel Campbell");
-			toPrint += p4.toString("902913944");
+			toPrint += p4.toString("ncampbell7");
 			//Person5 replace P4 with your name
 			//and gburdell5 with your gt id
 			Person5 p5 = new Person5("P5");
