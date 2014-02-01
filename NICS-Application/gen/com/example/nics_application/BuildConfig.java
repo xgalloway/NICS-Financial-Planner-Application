82648dc74a5ba7aaa6b2d1cfbe401d06e941d692
Zac <Zach@143.215.50.252>
2014-02-01 18:27:19 -0500

    Welcome and log in screen added. Emulator shows both ARE working on my system. C

---
+++ NICS-Application/gen/com/example/nics_application/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.nics_application;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- NICS-Application/src/com/example/nics_application/MainActivity.java
+++ NICS-Application/src/com/example/nics_application/MainActivity.java
@@ -2,15 +2,41 @@ package com.example.nics_application;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.*;
+import android.view.View.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	Button button;
+	EditText user;
+	EditText pass;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		addListenerOnButton();
 	}
+	
+	public void addListenerOnButton(){
+		button = (Button) findViewById(R.id.button1);
+		//String userString = user.getText().toString();
+		//String passString = pass.getText().toString();
+		
+		button.setOnClickListener(this);
+	}
+	
+	@Override
+	public void onClick(View v){				
+		Intent i = new Intent(this,LogInActivity.class);
+		startActivity(i);
+		
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
