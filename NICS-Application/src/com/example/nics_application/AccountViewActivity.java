fe0f6d648ba2e7859fe5bf27c472db563449b25a
Brent Satterwhite <bsatterwhite@gmail.com>
2014-02-06 00:00:06 -0500

    Additional files for login handling

---
+++ NICS-Application/src/com/example/nics_application/AccountViewActivity.java
@@ -0,0 +1,23 @@
+package com.example.nics_application;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+public class AccountViewActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account_view);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ NICS-Application/src/com/example/nics_application/User.java
@@ -0,0 +1,10 @@
+package com.example.nics_application;
+	
+public class User {
+	private String username;
+	private String password;
+	
+	public User(String name, String password) {
+		
+	}
+}
